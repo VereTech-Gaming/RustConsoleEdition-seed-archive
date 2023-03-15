d868c81d0d54c738bf1247d111d8d7eecf87c001
Jacob Rose <jacob.r.rose96@gmail.com>
2023-03-14 23:09:31 -0400

    Created top-level entry coordinator module.

---
+++ run.py
@@ -0,0 +1,13 @@
+"""
+This file will be a top-level coordinator for all processing, using:
+    crop.py:
+        for cropping of a single image, such as a vignetting template.
+    ffc.py:
+        for Flat Field Correction of a single image.
+    germ.py:
+        for (crop + ffc) image processing of one or all seeds.
+    plant.py:
+        for automated image stitching of one or all seeds.
+    prune.py:
+        for compression of one or all finalized seeds maps.
+"""
