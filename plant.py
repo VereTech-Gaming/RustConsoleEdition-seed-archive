cefb94d0e704f80392d5972e2bf647c310e1150d
Jacob Rose <jacob.r.rose96@gmail.com>
2023-03-14 22:56:42 -0400

    Created plant.py for Automated Image Stitching.

---
+++ plant.py
@@ -0,0 +1,11 @@
+"""This file is intended to be for the
+Automated Image Stitching portion of process.
+
+A seed is first "germinated".
+All raw images are prepared for stitching.
+
+A seed is then "planted".
+All fabricated images are stitched into a grid-aligned, cropped image.
+
+A plant is then "pruned".
+The final image is compressed to reduce transfer time and load."""
