8c2d92ef65c100fe4d1adc739bf92cbcfd2373f9
Jacob Rose <jacob.r.rose96@gmail.com>
2023-03-14 22:55:04 -0400

    Created prune.py to compress finished seed images.

---
+++ prune.py
@@ -0,0 +1,5 @@
+"""This script is to be run directly.
+It will iterate through all /live seeds, and attempt to compress
+any image with a filename sharing the current seed number.
+For example, if the iterator scans directory /5532,
+it will attempt to compress image file /5532/5532.png."""
